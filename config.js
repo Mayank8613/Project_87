d830b8257e9b0e04256ff5e4eb0312152973bb54
Mayank8613 <83433032+Mayank8613@users.noreply.github.com>
2022-07-21 00:29:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDG2Y-HOWrwGOwdkfHJlRGM3LnUrjDRZrw",
+  authDomain: "storytellingapp-3172d.firebaseapp.com",
+  databaseURL: "https://storytellingapp-3172d-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-3172d",
+  storageBucket: "storytellingapp-3172d.appspot.com",
+  messagingSenderId: "213789218374",
+  appId: "1:213789218374:web:a9c7709294b9d607bedc47"
+  };
